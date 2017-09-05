ffe844971d8e6addcd3a2def54ec5abad0ade87e
Claire Johnson <c.eliseee@gmail.com>
2017-09-05 12:22:49 -0700

    Working enemy movement animation

--- client/watchout.js
+++ client/watchout.js
@@ -4,23 +4,43 @@
 //   .attr("cx", 10).attr("cy", 10).attr("r", 10)
 //   .style("fill", "purple");
 
-const data = [];
 
 const boardWidth = 400;
 const boardHeight = 400;
-const enemies = 20;
-for (let i = 0; i < enemies; i++) {
-  const obj = {};
-  obj.x = Math.floor(Math.random() * boardWidth);
-  obj.y = Math.floor(Math.random() * boardHeight);
-  data[i] = obj;
-}
+const enemyCount = 20;
+const data = Array(enemyCount).fill({});
+
+const generatePosition = function() {
+  for (let i = 0; i < enemyCount; i++) {
+    const obj = {};
+    obj.x = Math.floor(Math.random() * boardWidth);
+    obj.y = Math.floor(Math.random() * boardHeight);
+    data[i] = obj;
+  }
+};
+
+generatePosition();
 
 
-svg = d3.select(".board").append("svg")
-      .attr("width", '' + boardWidth).attr("height",'' + boardHeight);
-svg.selectAll('circle').data(data).enter()
-  .append('circle')
-  .attr("cx", function(d) { return d.x; })
-  .attr("cy", function(d) { return d.y; })
-  .attr("r", 10);
+const svg = d3.select(".board").append("svg")
+            .attr("width", '' + boardWidth).attr("height",'' + boardHeight);
+const enemiesSVG = svg.selectAll('circle').data(data).enter()
+                      .append('circle')
+                      .attr("cx", function(d) { return d.x; })
+                      .attr("cy", function(d) { return d.y; })
+                      .attr("r", 10);
+
+const moveEnemies = () => {
+  setTimeout(() => {
+    enemiesSVG.transition().duration(700).attr('cx', function(d) {
+      return Math.floor(Math.random() * boardWidth);
+    })
+    .attr('cy', function(d) {
+      return Math.floor(Math.random() * boardHeight);
+    });
+
+    moveEnemies();
+  }, 1000);
+};
+
+moveEnemies();
